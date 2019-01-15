c1a1f606f36efda032f35e6f2b1b44acb486130b
rakhat tutueva and Slater Smith <rakhat@>
2019-01-15 14:21:34 -0800

    add ids and functions for fade in of flashcard list answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,113 @@
-$function() {
-  $("li").click(function() {
-  $("span").removeId("#java-script");
-  $().toggle();
-}
+$(function() {
+  $("#java-script1").click(function() {
+    $("#java-script").fadeIn();
+ });
+});
+
+$(function() {
+  $("#operators1").click(function() {
+    $("#operators").fadeIn();
+ });
+});
+
+$(function() {
+  $("#variables1").click(function() {
+    $("#variables").fadeIn();
+ });
+});
+
+$(function() {
+  $("#naming-convents1").click(function() {
+    $("#naming-convents").fadeIn();
+ });
+});
+
+$(function() {
+  $("#functions1").click(function() {
+    $("#functions").fadeIn();
+ });
+});
+
+$(function() {
+  $("#methods1").click(function() {
+    $("#methods").fadeIn();
+ });
+});
+
+$(function() {
+  $("#argument1").click(function() {
+    $("#argument").fadeIn();
+ });
+});
+
+$(function() {
+  $("#parameters1").click(function() {
+    $("#parameters").fadeIn();
+ });
+});
+
+$(function() {
+  $("#return1").click(function() {
+    $("#return").fadeIn();
+ });
+});
+
+$(function() {
+  $("#chaining1").click(function() {
+    $("#chaining").fadeIn();
+ });
+});
+
+$(function() {
+  $("#strings1").click(function() {
+    $("#strings").fadeIn();
+ });
+});
+
+$(function() {
+  $("#booleans1").click(function() {
+    $("#booleans").fadeIn();
+ });
+});
+
+$(function() {
+  $("#undefined1").click(function() {
+    $("#undefined").fadeIn();
+ });
+});
+
+$(function() {
+  $("#nan1").click(function() {
+    $("#nan").fadeIn();
+ });
+});
+
+$(function() {
+  $("#escape1").click(function() {
+    $("#escape").fadeIn();
+ });
+});
+
+$(function() {
+  $("#alert1").click(function() {
+    $("#alert").fadeIn();
+ });
+});
+
+$(function() {
+  $("#comments1").click(function() {
+    $("#comments").fadeIn();
+ });
+});
+
+$(function() {
+  $("#jquery1").click(function() {
+    $("#jquery").fadeIn();
+ });
+});
+
+$(function() {
+  $("#attributes1").click(function() {
+    $("#attributes").fadeIn();
+ });
+});
